688c82f7096e54830d56ae7bea62fde524772ab0
K <skwyha@yahoo.com>
2022-11-22 20:33:16 +0800

    Initialization of project

---
+++ src/main/java/one/aiouyang/community/AiouyangApplication.java
@@ -0,0 +1,13 @@
+package one.aiouyang.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiouyangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiouyangApplication.class, args);
+    }
+
+}
